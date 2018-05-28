b9d2e33c595b949beb16d683a265b74fc36244ba
dikshant2210 <dikshant2210@gmail.com>
2018-05-28 11:50:56 +0530

    data processing done

--- data_utils.py
+++ data_utils.py
@@ -1,10 +1,15 @@
 import dbcollection as dbc
 import cv2
 import numpy as np
-from utils import gaussian_image_map
+from utils import gaussian_image_map, plot_heatmap
 
 
 mpii = dbc.load('mpii_pose')
+filenames = mpii.get('train', 'image_filenames')
+keypoints = mpii.get('train', 'keypoints')
+objpos = mpii.get('train', 'objpos')
+scale = mpii.get('train', 'scale')
+keypoint_ids_per_image = mpii.get('train', 'list_keypoints_per_image')
 
 
 def ascii2str(array):
@@ -14,35 +19,59 @@ def ascii2str(array):
     return s
 
 
-def get_image_keypoints():
-    filenames = mpii.get('train', 'image_filenames')
-    keypoints = mpii.get('train', 'keypoints')
-    keypoint_ids_per_image = mpii.get('train', 'list_keypoints_per_image')
-
-    for name, keypoint_ids in zip(filenames[:1], keypoint_ids_per_image[:1]):
-        heatmap = list()
-        img = cv2.imread(ascii2str(name))
-        height, width = img.shape[0], img.shape[1]
-        indices = np.where(keypoint_ids != -1)[0]
-        keypoint_indices = np.take(keypoint_ids, indices)
-        for idx in keypoint_indices:
-            parts = keypoints[idx]
-            for part in parts:
-                if part[-1] != -1:
-                    # cv2.circle(img, (int(part[0]), int(part[1])), 1, (255, 0, 0), 2)
-                    gaussian_map = gaussian_image_map(height, width, int(part[0]), int(part[1]), 3.0)
-                    heatmap.append(gaussian_map)
-
-        heatmap = np.array(heatmap)
-        heatmap = np.max(heatmap, axis=0)
-        heatmap = np.expand_dims(heatmap, axis=-1)
-        heatmap = heatmap * 255.0
-        heatmap = heatmap.astype(np.uint8)
-        img += heatmap
-        print(heatmap.shape)
-        cv2.imshow('window', img)
-        # cv2.imshow('heatmap', heatmap)
-        cv2.waitKey(0)
+def create_heatmap(image, person_keypoints):
+    height, width = image.shape[0], image.shape[1]
+    heatmap = list()
+    for part in person_keypoints:
+        if part[-1] != -1:
+            gaussian_map = gaussian_image_map(height, width, int(part[0]), int(part[1]), 4.0)
+            heatmap.append(gaussian_map)
+        else:
+            heatmap.append(np.zeros((height, width)))
+    heatmap = np.array(heatmap)
+    heatmap = np.moveaxis(heatmap, 0, -1)
+    return heatmap
 
 
-get_image_keypoints()
+def train_generator():
+    file_index = list()
+    for index, val in enumerate(keypoint_ids_per_image):
+        num_persons = len(np.where(val != -1)[0])
+        for _ in range(num_persons):
+            file_index.append(index)
+
+    for index in range(len(file_index)):
+        image_path = ascii2str(filenames[file_index[index]])
+        person_keypoints = keypoints[index]
+
+        image = cv2.imread(image_path)
+
+        person_keypoints = person_keypoints[np.where((person_keypoints[:, 2] != -1) *
+                                                     (person_keypoints[:, 0] != 0) * (person_keypoints[:, 1] != 0))]
+        x_start = max(min(person_keypoints[:, 1]) - 100, 0)
+        x_end = min(max(person_keypoints[:, 1]) + 100, image.shape[0])
+        y_start = max(min(person_keypoints[:, 0]) - 100, 0)
+        y_end = min(max(person_keypoints[:, 0]) + 100, image.shape[1])
+
+        x_start, x_end, y_start, y_end = int(x_start), int(x_end), int(y_start), int(y_end)
+
+        person_keypoints[:, 0] = person_keypoints[:, 0] - y_start
+        person_keypoints[:, 1] = person_keypoints[:, 1] - x_start
+
+        crop_image = image[x_start:x_end, y_start:y_end, :]
+        height, width = crop_image.shape[0], crop_image.shape[1]
+        person_keypoints[:, 0] = person_keypoints[:, 0] * 368.0 / width
+        person_keypoints[:, 1] = person_keypoints[:, 1] * 368.0 / height
+
+        crop_image = cv2.resize(crop_image, (368, 368))
+        crop_heatmap = create_heatmap(crop_image, person_keypoints)
+        plot_heatmap(crop_image, crop_heatmap)
+
+
+if __name__ == '__main__':
+    print(filenames.shape)
+    print(keypoints.shape)
+    print(objpos.shape)
+    print(scale.shape)
+    print(keypoint_ids_per_image.shape)
+    train_generator()
--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import numpy as np
+import cv2
 
 
-heatmap_pixel_width = 10
+heatmap_pixel_width = 15
 
 
 def gaussian_image_map(img_height, img_width, c_y, c_x, variance):
@@ -13,5 +14,16 @@ def gaussian_image_map(img_height, img_width, c_y, c_x, variance):
             try:
                 image_map[x, y] = np.exp(-exponent)
             except IndexError:
-                print(x, y)
+                pass
     return image_map
+
+
+def plot_heatmap(image, heatmap):
+    heatmap = np.max(heatmap, axis=-1)
+    heatmap = (np.expand_dims(heatmap, axis=-1) * 255.0).astype(np.uint8)
+    heatmap_image = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    final_image = cv2.addWeighted(heatmap_image, 0.5, image, 0.5, 0)
+    cv2.imshow('final', final_image)
+    cv2.imshow('input', image)
+    cv2.imshow('heatmap', heatmap)
+    cv2.waitKey(0)
